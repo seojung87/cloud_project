e8d0762ad0a4407fbcdd0a48a34ca6e7ac306e2a
seojungyoo <seojungyoo22@gmail.com>
2023-06-19 03:02:02 +0900

    입력 폼 및 프런트 기능 완성

--- cashbook/views.py
+++ cashbook/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
 from .models import Cash, Category
+from datetime import datetime
 
 # Create your views here.
 def main(request):
@@ -15,6 +17,26 @@ def main(request):
 
 def input_form(request):
     actgr = Category.objects.all()
+
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        status = request.POST.get('btnradio')
+        category_name = request.POST.get('category')
+        memo = request.POST.get('memo')
+        date_str = request.POST.get('date')
+
+        category = Category.objects.get(name=category_name)
+
+        cash = Cash.objects.create(
+            amount=amount,
+            category=category,
+            memo=memo,
+            date=date_str,
+        )
+        cash.status = status
+        cash.save()
+
+        return redirect('cashbook/')
 
     return render(
         request,
