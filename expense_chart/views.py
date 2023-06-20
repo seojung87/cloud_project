b9db4567a11360d40426d3e9d9b36bb559899723
seojungyoo <seojungyoo22@gmail.com>
2023-06-21 08:26:51 +0900

    chart data 기간 반영하도록 수정 & about_me form 수정

--- expense_chart/views.py
+++ expense_chart/views.py
@@ -7,113 +7,132 @@ from cashbook.models import Cash
 
 # Create your views here.
 
-def export_income_data():
+def export_income_data(year):
     income = Cash.objects.filter(stat=True)
+    income = income.filter(date__year=year)
     monthly = [0] * 12
     for i in income :
         #print(i.date.month)
         monthly[i.date.month -1] += i.amount/10000
     #print(monthly)
     return monthly
-def export_expense_data():
+def export_expense_data(year):
     expense = Cash.objects.filter(stat=False)
+    expense = expense.filter(date__year=year)
     monthly = [0] * 12
     for i in expense:
         monthly[i.date.month - 1] += i.amount / 10000
     return monthly
 
-def category_div_1():
+def category_div_1(year, month):
     data = Cash.objects.filter(category="6")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_2():
+def category_div_2(year, month):
     data = Cash.objects.filter(category="7")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_3():
+def category_div_3(year, month):
     data = Cash.objects.filter(category="8")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_4():
+def category_div_4(year, month):
     data = Cash.objects.filter(category="9")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_5():
+def category_div_5(year, month):
     data = Cash.objects.filter(category="10")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_6():
+def category_div_6(year, month):
     data = Cash.objects.filter(category="11")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_7():
+def category_div_7(year, month):
     data = Cash.objects.filter(category="12")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_8():
+def category_div_8(year, month):
     data = Cash.objects.filter(category="13")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
-def category_div_9():
+def category_div_9(year, month):
     data = Cash.objects.filter(category="14")
+    data = data.filter(date__year=year)
+    data = data.filter(date__month=month)
     data_sum = 0
     for i in data:
         data_sum += i.amount
     return data_sum
 
-#퍼센트 구하느느 함수 추가하기
 def category_sum(c1,c2,c3,c4,c5,c6,c7,c8,c9):
     total_sum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9
     return total_sum
 
 @login_required
 def mainchart(request):
-    acash = Cash.objects.all()
-    amount_data_in = export_income_data()
-    json.dumps(amount_data_in)
-    amount_data_out = export_expense_data()
-    json.dumps(amount_data_out)
-
-    ct1 = category_div_1()
-    json.dumps(ct1)
-    ct2 = category_div_2()
-    json.dumps(ct2)
-    ct3 = category_div_3()
-    json.dumps(ct3)
-    ct4 = category_div_4()
-    json.dumps(ct4)
-    ct5 = category_div_5()
-    json.dumps(ct5)
-    ct6 = category_div_6()
-    json.dumps(ct6)
-    ct7 = category_div_7()
-    json.dumps(ct7)
-    ct8 = category_div_8()
-    json.dumps(ct8)
-    ct9 = category_div_9()
-    json.dumps(ct9)
-    total_sum = ct1 + ct2 + ct3 + ct4 + ct5 + ct6 + ct7 + ct8 + ct9
-    json.dumps(total_sum)
-
     year = datetime.now().year
     month = datetime.now().month
     month_name = calendar.month_name[month]
+
+    acash = Cash.objects.all()
+    amount_data_in = export_income_data(year)
+    json.dumps(amount_data_in)
+    amount_data_out = export_expense_data(year)
+    json.dumps(amount_data_out)
+
+    ct1 = category_div_1(year, month)
+    json.dumps(ct1)
+    ct2 = category_div_2(year, month)
+    json.dumps(ct2)
+    ct3 = category_div_3(year, month)
+    json.dumps(ct3)
+    ct4 = category_div_4(year, month)
+    json.dumps(ct4)
+    ct5 = category_div_5(year, month)
+    json.dumps(ct5)
+    ct6 = category_div_6(year, month)
+    json.dumps(ct6)
+    ct7 = category_div_7(year, month)
+    json.dumps(ct7)
+    ct8 = category_div_8(year, month)
+    json.dumps(ct8)
+    ct9 = category_div_9(year, month)
+    json.dumps(ct9)
+    total_sum = ct1 + ct2 + ct3 + ct4 + ct5 + ct6 + ct7 + ct8 + ct9
+    json.dumps(total_sum)
 
     return render(
         request,
